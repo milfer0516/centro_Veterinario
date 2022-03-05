ddd9f1c14cd69f6582cf4d1d1758f837c590585f
Milfer Muñoz <milfer0516@gmail.com>
2022-03-05 13:32:38 -0500

    Modifico los archivos js,css,index.html con la clase remove

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const mascotas = document.querySelector(".mascotas");
 
 navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visable");
+  if (!navMenu.classList.toggle("nav-menu_visable")) {
+    mascotas.classList.remove("mascotas_remove");
+  } else {
+    mascotas.classList.add("mascotas_remove");
+  }
 });
